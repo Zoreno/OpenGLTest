77b04662c0ec776a9767113abf6d3226a54090cf
Zoreno <metallica317@spray.se>
2017-02-09 02:19:50 +0100

    Lite fler kommentarer

--- OpenGL/ShaderProgram.h
+++ OpenGL/ShaderProgram.h
@@ -1,3 +1,10 @@
+/**
+ * @file	ShaderProgram.h
+ * @Author	Joakim Bertils
+ * @date	2017-02-09
+ * @brief	Shader Program interface
+ */
+
 #pragma once
 
 #include <string>
@@ -9,44 +16,144 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-
+/**
+ * @brief Shader Program Exception
+ */
 class ShaderProgramException : public std::runtime_error
 {
 public:
 	using std::runtime_error::runtime_error;
 };
 
+/**
+ * @brief Representation of a complete shader program.
+ */
 class ShaderProgram
 {
 public:
+
+	/**
+	 * @brief Default constructor.
+	 * @note Deleted. 
+	 */
 	ShaderProgram() = delete;
+
+	/**
+	 * @brief Constructor for the shader program.
+	 * @param vertexShaderPath Path to the vertex shader source.
+	 * @param fragmentShaderPath Path to the fragment shader source.
+	 */
 	ShaderProgram(const std::string& vertexShaderPath, const std::string& fragmentShaderPath);
+
+	/**
+	 * @brief Destructor.
+	 * 
+	 * Handles the memory cleanup for the shaders.
+	 */
 	~ShaderProgram();
 
+	/**
+	 * @brief Compile Shader
+	 * 
+	 * Compiles the vertex and fragment shader. It does not link the program.
+	 */
 	void compile();
+
+	/**
+	 * @brief Link Shader
+	 * 
+	 * Links the shader. Expects the vertex and fragment shaders to be loaded and compiled beforehand.
+	 */
 	void link() const;
 
+	/**
+	 * @brief Binds an attribute location for the shader.
+	 * @param index Location of the attribute.
+	 * @param name Name of the attribute.
+	 */
 	void bindAttribLocation(GLuint index, const GLchar* name) const;
 
+	/**
+	 * @brief Sets the openGL pipeline to use the shader for subsequent draws.
+	 */
 	void use() const;
+
+	/**
+	 * @brief Disables the shader.
+	 */
 	void disable() const;
 
+	/**
+	 * @brief Gets the openGL shader program handle.
+	 * @return Program Handle.
+	 */
 	GLuint getShaderProgramHandle() const;
 
-	// Add more as needed
-
+	/**
+	 * @brief Uploads a value as an uniform to the shader.
+	 * @param name Name of the uniform.
+	 * @param value Value to be upload.
+	 */
 	void uploadUniform(const std::string& name, float value);
+
+	/**
+	* @brief Uploads a value as an uniform to the shader.
+	* @param name Name of the uniform.
+	* @param value Value to be upload.
+	*/
 	void uploadUniform(const std::string& name, int value);
+
+	/**
+	* @brief Uploads a value as an uniform to the shader.
+	* @param name Name of the uniform.
+	* @param value Value to be upload.
+	*/
 	void uploadUniform(const std::string& name, glm::vec2 value);
+
+	/**
+	* @brief Uploads a value as an uniform to the shader.
+	* @param name Name of the uniform.
+	* @param value Value to be upload.
+	*/
 	void uploadUniform(const std::string& name, glm::vec3 value);
+
+	/**
+	* @brief Uploads a value as an uniform to the shader.
+	* @param name Name of the uniform.
+	* @param value Value to be upload.
+	*/
 	void uploadUniform(const std::string& name, glm::vec4 value);
+
+	/**
+	* @brief Uploads a value as an uniform to the shader.
+	* @param name Name of the uniform.
+	* @param value Value to be upload.
+	*/
 	void uploadUniform(const std::string& name, glm::mat4 value);
 private:
 
+	/**
+	 * @brief Path to the vertex shader source.
+	 */
 	std::string vertexShaderPath;
+
+	/**
+	 * @brief Path to the fragment shader source.
+	 */
 	std::string fragmentShaderPath;
+
+	/**
+	 * @brief OpenGL shader program handle.
+	 */
 	GLuint shaderProgramHandle;
+
+	/**
+	 * @brief OpenGL vertex shader handle.
+	 */
 	GLuint vertexShaderHandle;
+
+	/**
+	 * @brief OpenGL fragment shader handle.
+	 */
 	GLuint fragmentShaderHandle;
 };
-
